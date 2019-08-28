d2b962db44fd4cc3e26a92c358c44748fdbb724c
lyle dickie <lyle.e.dickie@gmail.com>
2019-08-28 19:40:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,8 +3,8 @@ def second_challenge
     :montague => {
       :patriarch => '', :matriarch => '', :hero => '', :hero_friends => ''
       }, 
-    :capulet => { :patriarch => '', :matriarch => '', :heroine => '', :heroine_friends => ''
-      #your key/value pairs here!
+    :capulet => { 
+      :patriarch => '', :matriarch => '', :heroine => '', :heroine_friends => ''
       }}
 
   
